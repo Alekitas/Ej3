0c4b90b0fa2f9e0c52702ef903d61dbf129e0ad1
Alejo Atienza <73915312+Alekitas@users.noreply.github.com>
2022-04-26 13:49:14 -0300

    Add files via upload

--- Ej3/ClaseRegistro.py
+++ Ej3/ClaseRegistro.py
@@ -7,5 +7,43 @@ class Registro:
         self.__temperatura=temperatura
         self.__humedad=humedad
         self.__presatmos=presatmos
-    def gettemperatura(self):
+        
+    def mostrarDatos(self):
+        print (str(self.__temperatura)+','+str(self.__humedad)+','+str(self.__presatmos))
+    def getTemperatura(self):
         return self.__temperatura
+    def gethumedad(self):
+        return self.__humedad
+    def getpresatmos(self):
+        return self.__presatmos
+    
+    def tempmaxima(self,lista,horas,dia):
+        maximo=-9999
+        for dia in range(len(lista)):
+            for horas in range(len(lista[dia])):
+                if lista[dia-1][horas].getTemperatura()>maximo:
+                    maximo=lista[dia-1][horas].getTemperatura()
+        print('Temperatura maxima: {} dia: {} hora: {}'.format(maximo,dia,horas))
+    def tempminima(self,lista):
+        minimo=99999
+        for i in range(len(lista)):
+            for j in range(len(lista[i])):
+                if lista[i][j].gettemperatura()<minimo:
+                    minimo=lista[i][j].gettemperatura
+                    
+    def hummaxima(self,lista):
+        maximo=-9999
+        for i in range(len(lista)):
+            for j in range(len(lista[i])):
+                if lista[i][j].gethumedad()>maximo:
+                    maximo=lista[i][j].gethumedad()
+    def humminima(self,lista):
+        minimo=99999
+        for i in range(len(lista)):
+            for j in range(len(lista[i])):
+                if lista[i][j].gethumedad()<minimo:
+                    minimo=lista[i][j].gethumedad()
+    def presminima(self):
+        pass
+    def presmaxima(self):
+        pass
--- Ej3/Main.py
+++ Ej3/Main.py
@@ -3,17 +3,24 @@ from ClaseRegistro import Registro
 import csv
 
 if __name__=='__main__':
-    dias = 30
-    horas = 24
+    dia = 31
+    horas = 23
     lista=[]
-    for i in range(horas):
-        lista.append([0]*dias)
-    archivo=open('archivo.csv')
+    for i in range(dia):
+        lista.append([0]*horas)
+    archivo=open('archivo1.csv')
     reader=csv.reader(archivo,delimiter=';')
+    bandera=True
     for linea in reader:
-        dia=int(linea[0])
-        hora=int(linea[1])
-        temperatura=linea[2]
-        humedad=linea[3]
-        presatmos=linea[4]
-        lista[hora][dia-1] = Registro(temperatura,humedad,presatmos)
+        if bandera:
+            #salteo primera linea
+            bandera=False
+        else:
+            dia=int(linea[0])   
+            horas=int(linea[1])
+            temperatura=float(linea[2])
+            humedad=int(linea[3])
+            presatmos=int(linea[4])
+            lista[dia-1][horas-1]=Registro(temperatura,humedad,presatmos)
+    lista[0][0].mostrarDatos()
+    lista[dia-1][horas-1].tempmaxima(lista,horas,dia)
